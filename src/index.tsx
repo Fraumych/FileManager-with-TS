e2097fd695ff16980489272629d17ba858189f7f
Fraumych <laxus1991@ukr.net>
2022-11-10 16:58:15 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDom from "react-dom/client";
 import App from "./App";
+import "../src/bootstrap/css/bootstrap.css";
 
 ReactDom.createRoot(document.getElementById("root")!).render(<App />);
