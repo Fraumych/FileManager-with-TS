6d0ecdaeeff6fc9be383900129a04fa8ba342f43
Fraumych <laxus1991@ukr.net>
2022-11-10 15:16:56 +0200

    path

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDom from "react-dom/client";
 import App from "./App";
-import "./bootstrap/css/bootstrap.css";
+import "../src/bootstrap/css/bootstrap.css";
 
 ReactDom.createRoot(document.getElementById("root")!).render(<App />);
