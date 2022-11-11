0be992bd363e5be9408a72497a23f8a4bdcc22f6
Fraumych <laxus1991@ukr.net>
2022-11-11 14:32:23 +0200

    update

--- src/Page/Login.tsx
+++ src/Page/Login.tsx
@@ -9,7 +9,7 @@ const Login: React.FC = () => {
 
    const { isAuth } = useContext(UserContext);
 
-   const urlAuth = "https://www.dropbox.com/1/oauth2/authorize?client_id=xnumlhdrd6w4xcb&response_type=code&token_access_type=offline&redirect_uri=https://fraumych.github.io/file&state=<CSRF token>";
+   const urlAuth = "https://www.dropbox.com/1/oauth2/authorize?client_id=xnumlhdrd6w4xcb&response_type=code&token_access_type=offline&redirect_uri=://fraumych.github.io/file&state=<CSRF token>";
 
    useEffect(() => {
       if (isAuth) {
